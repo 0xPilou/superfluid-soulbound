709449d11430d246d071a0b6dfc0f93ec55a6d02
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-13 09:57:29 +0200

    Add soulbound to typechain index

--- packages/web3-config/index.ts
+++ packages/web3-config/index.ts
@@ -1,15 +1,21 @@
 import MyNFTDeployment from "./deployments/goerli/MyNFT.json";
 import CashflowDeployment from "./deployments/optimismGoerli/Cashflow.json";
+import SoulboundDeployment from "./deployments/optimismGoerli/Soulbound.json";
 
 export * from "./typechain";
 import * as _typechain from "./typechain";
 import { chain } from "wagmi";
 
-import { MyNFT__factory, Cashflow__factory } from "./typechain";
+import {
+  MyNFT__factory,
+  Cashflow__factory,
+  Soulbound__factory,
+} from "./typechain";
 
 export const typechain = _typechain;
 
 export type AvailableContracts =
+  | Soulbound__factory["contractName"]
   | MyNFT__factory["contractName"]
   | Cashflow__factory["contractName"];
 
@@ -17,6 +23,7 @@ type AddressObj = Record<AvailableContracts, string>;
 
 const _myNft = new MyNFT__factory();
 const _cashflow = new Cashflow__factory();
+const _soulbound = new Soulbound__factory();
 
 export const Address: Record<number, Partial<AddressObj>> = {
   [chain.goerli.id]: {
@@ -24,6 +31,9 @@ export const Address: Record<number, Partial<AddressObj>> = {
   },
   [chain.optimismGoerli.id]: {
     [_cashflow.contractName]: CashflowDeployment.address,
+  },
+  [chain.optimismGoerli.id]: {
+    [_soulbound.contractName]: SoulboundDeployment.address,
   },
 };
 
