1868fe5bb57a00986de2ac1c47851481a43ce174
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-13 09:14:08 +0200

    push new contracts

---
+++ apps/smart-contracts/contracts/Soulbound.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Soulbound is ERC20, AccessControl {
+    bytes32 public constant MINTER = keccak256("MINTER");
+    bytes32 public constant BURNER = keccak256("BURNER");
+
+    error NOT_MINTER();
+    error NOT_BURNER();
+    error NOT_TRANSFERABLE();
+
+    constructor()
+        ERC20("Soulbound Token", "SBT")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyRole(MINTER){
+        _mint(_to, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) external onlyRole(BURNER){
+        _burn(_from, _amount);
+    }
+
+    function transfer(address _to, uint256 _amount)
+        public
+        override
+        returns (bool)
+    {
+        revert NOT_TRANSFERABLE();
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) public override returns (bool) {
+        revert NOT_TRANSFERABLE();
+    }
+}
---
+++ apps/smart-contracts/test/soulbound-tests.ts
@@ -0,0 +1,127 @@
+import { ethers } from "hardhat";
+import chai from "chai";
+import { Soulbound } from "../typechain";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+const { expect } = chai;
+
+describe("Soulbound", () => {
+  let soulbound: Soulbound;
+
+  let owner: SignerWithAddress;
+  let minter: SignerWithAddress;
+  let burner: SignerWithAddress;
+  let user1: SignerWithAddress;
+  let user2: SignerWithAddress;
+  let addrs: SignerWithAddress[];
+
+  beforeEach(async () => {
+    [owner, minter, burner, user1, user2, ...addrs] = await ethers.getSigners();
+
+    const Soulbound = await ethers.getContractFactory("Soulbound");
+
+    soulbound = (await Soulbound.deploy()) as Soulbound;
+
+    const minter_role = await soulbound.MINTER();
+    const burner_role = await soulbound.BURNER();
+    await soulbound.connect(owner).grantRole(minter_role, minter.address);
+    await soulbound.connect(owner).grantRole(burner_role, burner.address);
+  });
+
+  describe("METHOD : mint", async () => {
+    it("should mint SBT token to user", async () => {
+      await soulbound
+        .connect(minter)
+        .mint(user1.address, ethers.utils.parseEther("1"));
+      expect(await soulbound.balanceOf(user1.address)).to.eq(
+        ethers.utils.parseEther("1")
+      );
+    });
+
+    it("should not be able to mint SBT token to self", async () => {
+      await expect(
+        soulbound
+          .connect(user1)
+          .mint(user1.address, ethers.utils.parseEther("1"))
+      ).to.be.reverted;
+    });
+
+    it("should not be able to mint SBT token to another user", async () => {
+      await expect(
+        soulbound
+          .connect(user1)
+          .mint(user2.address, ethers.utils.parseEther("1"))
+      ).to.be.reverted;
+    });
+  });
+
+  describe("METHOD : transfer", async () => {
+    before(async () => {
+      await soulbound
+        .connect(minter)
+        .mint(user1.address, ethers.utils.parseEther("10"));
+    });
+
+    it("should not be able to transfer SBT token to another user", async () => {
+      await expect(
+        soulbound
+          .connect(user1)
+          .transfer(user2.address, ethers.utils.parseEther("5"))
+      ).to.be.revertedWith("NOT_TRANSFERABLE");
+    });
+  });
+
+  describe("METHOD : transferFrom", async () => {
+    before(async () => {
+      await soulbound
+        .connect(minter)
+        .mint(user1.address, ethers.utils.parseEther("10"));
+    });
+    it("should not be able to transfer SBT token from another user (even if approved)", async () => {
+      await soulbound
+        .connect(user1)
+        .approve(user2.address, ethers.utils.parseEther("2"));
+      await expect(
+        soulbound
+          .connect(user2)
+          .transferFrom(
+            user1.address,
+            user2.address,
+            ethers.utils.parseEther("1")
+          )
+      ).to.be.revertedWith("NOT_TRANSFERABLE");
+    });
+  });
+
+  describe("METHOD : burn", async () => {
+    beforeEach(async () => {
+      await soulbound
+        .connect(minter)
+        .mint(user1.address, ethers.utils.parseEther("10"));
+    });
+    it("should burn SBT token from user", async () => {
+      await soulbound
+        .connect(burner)
+        .burn(user1.address, ethers.utils.parseEther("1"));
+      expect(await soulbound.balanceOf(user1.address)).to.eq(
+        ethers.utils.parseEther("9")
+      );
+    });
+
+    it("should not be able to burn SBT token from self", async () => {
+      await expect(
+        soulbound
+          .connect(user1)
+          .burn(user1.address, ethers.utils.parseEther("1"))
+      ).to.be.reverted;
+    });
+
+    it("should not be able to burn SBT token from another user", async () => {
+      await expect(
+        soulbound
+          .connect(user1)
+          .burn(user2.address, ethers.utils.parseEther("1"))
+      ).to.be.reverted;
+    });
+  });
+});
