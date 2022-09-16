843647835a95396740b892e0e515b57b509aff5e
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-16 15:55:31 +0200

    deploy script

--- apps/smart-contracts/deploy/deploy-store.ts
+++ apps/smart-contracts/deploy/deploy-store.ts
@@ -2,38 +2,22 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
 
-const name = "SuperSoulbound";
-
-const superTokenFactory = "0x9aCc39d15e3f168c111a1D4F80271a9E526c9a9F";
+const name = "Store";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, network } = hre;
   const { deployer } = await getNamedAccounts();
   const { deploy } = deployments;
 
-  const store = await deployments.get("Store");
-
   const deployment = await deploy(name, {
     from: deployer,
-    args: [store.address],
+    args: [],
   });
 
   deployments.log(`Contract ${name} deployed at ${deployment.address}`);
-
-  const superSoulbound = await hre.ethers.getContractAt(
-    name,
-    deployment.address
-  );
-
-  await superSoulbound.initialize(
-    "FakeSuperSoulBound #0",
-    "fSB0x",
-    ethers.utils.getAddress(superTokenFactory)
-  );
 };
 
 func.tags = [name];
 func.skip = async (env) => env.network.name !== "optimismGoerli";
-func.dependencies = ["Store"];
 
 export default func;
