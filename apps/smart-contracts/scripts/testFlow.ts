9f2ef526ed0014ed73ee05ff6245da206c587467
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-19 09:46:43 +0200

    script to test if user can create flows

---
+++ apps/smart-contracts/scripts/testFlow.ts
@@ -0,0 +1,30 @@
+import { ethers } from "ethers";
+import { Framework } from "@superfluid-finance/sdk-core";
+
+const goerliOptimismResolver = "0x21d4E9fbB9DB742E6ef4f29d189a7C18B0b59136";
+
+const url = `https://opt-goerli.g.alchemy.com/v2/${process.env.OPTIMISM_GOERLI_ALCHEMY_KEY}`;
+const provider = new ethers.providers.JsonRpcProvider(url);
+
+const main = async () => {
+  const sf = await Framework.create({
+    provider: provider,
+    chainId: 420,
+    resolverAddress: goerliOptimismResolver,
+  });
+
+  const wallet = new ethers.Wallet(`${process.env.PKEY_2}`, provider);
+  const signer = sf.createSigner({ signer: wallet });
+
+  const createFlowOperation = sf.cfaV1.createFlow({
+    receiver: "0x18CCC193FeBDAf93A2C5e24E306E72a77012C429",
+    superToken: "0xC591aA96E79a0c980F74482Da3479b4e207Bb768",
+    flowRate: ethers.utils.parseEther("1").toString(),
+  });
+  const txn = await createFlowOperation.exec(signer);
+  const res = await txn.wait();
+
+  console.log("Flow Created : ", res);
+};
+
+main();
