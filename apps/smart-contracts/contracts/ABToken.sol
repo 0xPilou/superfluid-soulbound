6b299fa33af729ce5e8ef64b455a5b1537eeafbd
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-21 15:21:07 +0200

    draft ABToken

---
+++ apps/smart-contracts/contracts/ABToken.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: AGPLv3
+pragma solidity ^0.8.16;
+
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { SuperSoulbound } from "./SuperSoulbound.sol";
+
+contract ABToken is SuperSoulbound, AccessControl {
+  address public store;
+
+  bytes32 public constant BURNER = keccak256("BURNER");
+  bytes32 public constant MINTER = keccak256("MINTER");
+
+  constructor(address _store, address _host) SuperSoulbound(_host) {
+    store = _store;
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _grantRole(MINTER, msg.sender);
+    _grantRole(BURNER, _store);
+  }
+
+  function mint(address _receiver, uint256 _amount) external onlyRole(MINTER) {
+    _mint(msg.sender, _receiver, _amount, false, "0x", new bytes(0));
+  }
+
+  function burn(address _from, uint256 _amount) external onlyRole(BURNER) {
+    _burn(msg.sender, _from, _amount, "0x", new bytes(0));
+  }
+
+  function setStore(address _store) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    require(_store != address(0), "zero-address");
+    store = _store;
+    _grantRole(BURNER, _store);
+  }
+}
