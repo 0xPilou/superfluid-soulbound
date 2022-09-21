082e48085d20870e7a2eb93403d54b569b37bf32
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-21 15:21:02 +0200

    review scripts

--- apps/smart-contracts/deploy/deploy-superSoulbound.ts
+++ apps/smart-contracts/deploy/deploy-superSoulbound.ts
@@ -3,36 +3,32 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
 import { STORE_NAME, SUPERSOULBOUND_NAME } from "../deploy-constants";
 
-const superTokenFactory = "0x9aCc39d15e3f168c111a1D4F80271a9E526c9a9F";
+const SF_HOST_ADDRESS = "0xE40983C2476032A0915600b9472B3141aA5B5Ba9";
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, network } = hre;
   const { deployer } = await getNamedAccounts();
   const { deploy } = deployments;
 
-  const store = await deployments.get(STORE_NAME);
-
   const deployment = await deploy(SUPERSOULBOUND_NAME, {
     from: deployer,
-    args: [store.address],
+    args: [SF_HOST_ADDRESS],
   });
 
-  deployments.log(`Contract ${SUPERSOULBOUND_NAME} deployed at ${deployment.address}`);
+  deployments.log(
+    `Contract ${SUPERSOULBOUND_NAME} deployed at ${deployment.address}`
+  );
 
   const superSoulbound = await hre.ethers.getContractAt(
     SUPERSOULBOUND_NAME,
     deployment.address
   );
 
-  await superSoulbound.initialize(
-    "FakeSuperSoulBound #0",
-    "fSB0x",
-    ethers.utils.getAddress(superTokenFactory)
-  );
+  await superSoulbound.initialize(ZERO_ADDRESS, 0, "SuperSoulboundV1", "SBV1x");
 };
 
 func.tags = [SUPERSOULBOUND_NAME];
-func.dependencies = [STORE_NAME];
 func.skip = async (env) => env.network.name !== "optimismGoerli";
 
 export default func;
--- apps/smart-contracts/scripts/mintSBT.ts
+++ apps/smart-contracts/scripts/mintSBT.ts
@@ -17,11 +17,7 @@ const main = async () => {
 
   await sbt
     .connect(deployer)
-    .mint(
-      CashflowDeployment.address,
-      ethers.utils.parseEther("1000000000"),
-      "0x"
-    );
+    .mint(CashflowDeployment.address, ethers.utils.parseEther("1000000000"));
 
   console.log(
     `Minted 1.000.000.000 SBTx to Cashflow contract at ${CashflowDeployment.address}`
