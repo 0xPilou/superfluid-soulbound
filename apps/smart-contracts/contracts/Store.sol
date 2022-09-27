a907bc8fb7499b9a19ecaa148bde861b9b11c0dd
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-27 09:22:18 +0200

    update Store contract

--- apps/smart-contracts/contracts/Store.sol
+++ apps/smart-contracts/contracts/Store.sol
@@ -19,12 +19,13 @@ contract Store is ERC1155, Ownable {
   }
   error OutOfStock();
   error IncorrectItem();
+  error IncorrectParameters();
 
   mapping(uint256 => Item) public items;
 
   event UpdatedInventory(uint256 itemId, uint256 quantity, uint256 price);
-  event Purchased(address buyer, uint256 itemId, uint256 quantity);
-  event Redeemed(address buyer, uint256 itemId, uint256 quantity);
+  event Purchased(address buyer, uint256[] itemIds, uint256[] quantities);
+  event Redeemed(address buyer, uint256[] itemIds, uint256[] quantities);
 
   constructor() ERC1155("") {}
 
@@ -32,21 +33,29 @@ contract Store is ERC1155, Ownable {
    *                            PUBLIC FUNCTIONS
    *************************************************************************/
 
-  function purchase(uint256 _itemId, uint256 _quantity) external {
-    Item storage item = items[_itemId];
+  function purchase(uint256[] memory _itemIds, uint256[] memory _quantities)
+    external
+  {
+    if (_itemIds.length != _quantities.length) revert IncorrectParameters();
+    uint256 totalPrice = 0;
+    for (uint256 i = 0; i < _itemIds.length; ++i) {
+      Item storage item = items[_itemIds[i]];
+      if (item.quantity < _quantities[i]) revert OutOfStock();
+      item.quantity -= _quantities[i];
+      totalPrice += _quantities[i] * item.price;
+    }
+    IABToken(token).burn(msg.sender, totalPrice);
+    _mintBatch(msg.sender, _itemIds, _quantities, "");
 
-    if (item.quantity < _quantity) revert OutOfStock();
-
-    IABToken(token).burn(msg.sender, _quantity * item.price);
-    item.quantity -= _quantity;
-
-    _mint(msg.sender, _itemId, _quantity, "");
-    emit Purchased(msg.sender, _itemId, _quantity);
+    emit Purchased(msg.sender, _itemIds, _quantities);
   }
 
-  function redeem(uint256 _itemId, uint256 _quantity) external {
-    _burn(msg.sender, _itemId, _quantity);
-    emit Redeemed(msg.sender, _itemId, _quantity);
+  function redeem(uint256[] memory _itemIds, uint256[] memory _quantities)
+    external
+  {
+    if (_itemIds.length != _quantities.length) revert IncorrectParameters();
+    _burnBatch(msg.sender, _itemIds, _quantities);
+    emit Redeemed(msg.sender, _itemIds, _quantities);
   }
 
   /**************************************************************************
