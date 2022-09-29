b73bdfc7804730b6d4b5146da024a9d380024fc6
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-29 10:38:04 +0200

    add qty selection for mint view

--- apps/web/src/components/MintView.tsx
+++ apps/web/src/components/MintView.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { useAccount } from "wagmi";
 import { useContractRead, useContractWrite } from "wagmi-lfg";
 import { MyNFT__factory } from "web3-config";
 const MintView = () => {
   const { address } = useAccount();
+  const [quantity, setQuantity] = useState(1);
 
   const { data: mintCount } = useContractRead(MyNFT__factory, "mintCount");
   const { data: minted } = useContractRead(MyNFT__factory, "balanceOf", {
@@ -10,13 +12,21 @@ const MintView = () => {
     enabled: Boolean(address),
   });
   const { write: mint } = useContractWrite(MyNFT__factory, "mintNft", {
-    args: [address, 1],
+    args: [address, quantity],
     onSuccessMessage: "minted!",
   });
+
+  const handleChangeQuantity = (event) => {
+    setQuantity(event.target.value);
+  };
 
   return (
     <div>
       {(mintCount && mintCount?.toString()) || null} / 100
+      <div>
+        <label>Quantity :</label>
+        <input value={quantity} onChange={handleChangeQuantity} />
+      </div>
       <button onClick={() => mint()}>MINT</button>
       {address && minted && <div>You have minted: {minted?.toString()}</div>}
     </div>
