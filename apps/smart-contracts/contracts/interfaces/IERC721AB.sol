707fb190e261484723227d52e5611c9a47151d97
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-30 11:16:13 +0200

    copied ABv1 contracts (integration start)

---
+++ apps/smart-contracts/contracts/interfaces/IERC721AB.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IERC721AB {
+    /**
+     * @notice
+     *  Update anotherblock address
+     *  Revoke the AB_ROLE from the previous anotherblock address
+     *  Grant the AB_ROLE role to the new address
+     *  Only DEFAULT_ADMIN_ROLE can perform this operation
+     *
+     * @param _anotherblock : new anotherblock address
+     */
+    function setAnotherblock(address _anotherblock) external;
+
+    /**
+     * @notice
+     *  Return an array containing the token IDs owned by the given address
+     *
+     * @param _owner : owner address
+     * @return result : array containing all the token IDs owned by `_owner`
+     */
+    function tokensOfOwner(address _owner)
+        external
+        view
+        returns (uint256[] memory);
+
+    function dropIdPerToken(uint256 _tokenId) external returns (uint256);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external;
+}
