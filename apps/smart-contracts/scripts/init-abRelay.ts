26446da2d89cfad3e0ecf966be9b8c7bf79a3e6f
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-09-30 14:45:00 +0200

    update relay init script

--- apps/smart-contracts/scripts/init-abRelay.ts
+++ apps/smart-contracts/scripts/init-abRelay.ts
@@ -3,7 +3,8 @@ import hre from "hardhat";
 import ABRelayDeployment from "web3-config/deployments/optimismGoerli/ABRelay.json";
 import ABStreamDeployment from "web3-config/deployments/optimismGoerli/ABStream.json";
 import ABRegistryDeployment from "web3-config/deployments/optimismGoerli/ABRegistry.json";
-import NFTDeployment from "web3-config/deployments/goerli/MyNFT.json";
+import ABNFTDeployment from "web3-config/deployments/goerli/ABNFT.json";
+import ABDropManagerDeployment from "web3-config/deployments/goerli/ABDropManager.json";
 
 const main = async () => {
   console.log(
@@ -21,11 +22,20 @@ const main = async () => {
 
   let tx = await abRelay
     .connect(deployer)
-    .grantAllowance(NFTDeployment.address);
+    .grantAllowance(ABNFTDeployment.address);
   await tx.wait();
 
   console.log(
-    `ABRelay : Granted allowance to write message to address : ${NFTDeployment.address}`
+    `ABRelay : Granted allowance to write message to address : ${ABNFTDeployment.address}`
+  );
+
+  tx = await abRelay
+    .connect(deployer)
+    .grantAllowance(ABDropManagerDeployment.address);
+  await tx.wait();
+
+  console.log(
+    `ABRelay : Granted allowance to write message to address : ${ABDropManagerDeployment.address}`
   );
 
   tx = await abRelay.connect(deployer).setABStream(ABStreamDeployment.address);
