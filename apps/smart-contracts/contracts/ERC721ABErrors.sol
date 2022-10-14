a70159d929b18755646fa3951912c73fe69cf269
Pilou <76021631+0xPilou@users.noreply.github.com>
2022-10-14 16:44:23 +0200

    added errors

--- apps/smart-contracts/contracts/ERC721ABErrors.sol
+++ apps/smart-contracts/contracts/ERC721ABErrors.sol
@@ -6,23 +6,23 @@
 //                            ██████████████████████████████████
 //                            ██████████████████████████████████
 //                            ██████████████████████████████████
-//                            ██████████████████████████████████    
-//                            ████████████████████████          ██████████        
-//                            ████████████████████████          ██████████        
-//                            ████████████████████████          ██████████        
-//                            ████████████████████████          ██████████        
-//                                                    ████████████████████ 
-//                                                    ████████████████████ 
-//                                                    ████████████████████            
+//                            ██████████████████████████████████
+//                            ████████████████████████          ██████████
+//                            ████████████████████████          ██████████
+//                            ████████████████████████          ██████████
+//                            ████████████████████████          ██████████
+//                                                    ████████████████████
+//                                                    ████████████████████
+//                                                    ████████████████████
 //                                                    ████████████████████
 //
 //
 //  █████╗ ███╗   ██╗ ██████╗ ████████╗██╗  ██╗███████╗██████╗ ██████╗ ██╗      ██████╗  ██████╗██╗  ██╗
 // ██╔══██╗████╗  ██║██╔═══██╗╚══██╔══╝██║  ██║██╔════╝██╔══██╗██╔══██╗██║     ██╔═══██╗██╔════╝██║ ██╔╝
-// ███████║██╔██╗ ██║██║   ██║   ██║   ███████║█████╗  ██████╔╝██████╔╝██║     ██║   ██║██║     █████╔╝ 
-// ██╔══██║██║╚██╗██║██║   ██║   ██║   ██╔══██║██╔══╝  ██╔══██╗██╔══██╗██║     ██║   ██║██║     ██╔═██╗ 
+// ███████║██╔██╗ ██║██║   ██║   ██║   ███████║█████╗  ██████╔╝██████╔╝██║     ██║   ██║██║     █████╔╝
+// ██╔══██║██║╚██╗██║██║   ██║   ██║   ██╔══██║██╔══╝  ██╔══██╗██╔══██╗██║     ██║   ██║██║     ██╔═██╗
 // ██║  ██║██║ ╚████║╚██████╔╝   ██║   ██║  ██║███████╗██║  ██║██████╔╝███████╗╚██████╔╝╚██████╗██║  ██╗
-// ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝                                                                                             
+// ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝
 //
 /**
  * @title ERC721ABErrors
@@ -34,43 +34,53 @@
 pragma solidity ^0.8.4;
 
 contract ERC721ABErrors {
-    /**
+  /**
     @notice Error returned if the drop is sold-out
     **/
-    error DropSoldOut();
+  error DropSoldOut();
 
-    /**
+  /**
     @notice Error returned if the whitelist sale has not started yet
     **/
-    error SaleNotStarted();
+  error SaleNotStarted();
 
-    /**
+  /**
     @notice Error returned if user attempt to mint more than allowed by the drop
     **/
-    error MaxMintPerAddress();
+  error MaxMintPerAddress();
 
-    /**
+  /**
     @notice Error returned if the supply is sold out
     **/
-    error NotEnoughTokensAvailable();
+  error NotEnoughTokensAvailable();
 
-    /**
+  /**
     @notice Error returned if user did not send the correct amount of ETH
     **/
-    error IncorrectETHSent();
+  error IncorrectETHSent();
 
-    /**
+  /**
     @notice Error returned if non-whitelisted user attempts to mint prior to public sale  
     **/
-    error NotInMerkle();
+  error NotInMerkle();
 
-    /**
+  /**
     @notice Error returned if the contract passed as parameters does not implement the expected interface  
     **/
-    error IncorrectInterface();
+  error IncorrectInterface();
 
-    /**
+  /**
     @notice Error returned if there are no ETH to withdraw from the contract
     **/
-    error NothingToWithdraw();
+  error NothingToWithdraw();
+
+  /**
+    @notice Error returned if the caller is not authorized
+    **/
+  error Forbidden();
+
+  /**
+    @notice Error returned when a ETH transfer failed
+    **/
+  error TransferFailed();
 }
